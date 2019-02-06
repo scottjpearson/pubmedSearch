07d85f3ea1d8b99cdcdd64213487b479a2020e03
Scott J. Pearson <scott.j.pearson@vumc.org>
2019-02-06 12:53:36 -0600

    show

--- PubmedSearchExternalModule.php
+++ PubmedSearchExternalModule.php
@@ -4,7 +4,7 @@ namespace Vanderbilt\PubmedSearchExternalModule;
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 
-define('SLEEP_TIME', 400);
+define('SLEEP_TIME', 1);
 
 class PubmedSearchExternalModule extends AbstractExternalModule
 {
@@ -172,7 +172,7 @@ class PubmedSearchExternalModule extends AbstractExternalModule
 					curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
 					$output = curl_exec($ch);
 					curl_close($ch);
-					usleep(SLEEP_TIME);
+					sleep(SLEEP_TIME);
 	
 					$pmData = json_decode($output, true);
 					if ($pmData['esearchresult'] && $pmData['esearchresult']['idlist']) {
@@ -224,7 +224,7 @@ class PubmedSearchExternalModule extends AbstractExternalModule
 				curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
 				$output = curl_exec($ch);
 				curl_close($ch);
-				usleep(SLEEP_TIME);
+				sleep(SLEEP_TIME);
 				$data = json_decode($output, true);
 	
 				# indexed by PMID
